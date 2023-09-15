b92b07505aa2239ef2ce214531309fc22dcb6800
Juan Carlos Valdés <idevkingos@gmail.com>
2023-09-15 13:53:07 -0300

    config circleci

--- next.config.js
+++ next.config.js
@@ -3,6 +3,9 @@ const path = require('path')
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  env: {
+    NEXTAUTH_URL: process.env.NEXTAUTH_URL,
+  },
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
     prependData: `@import "./base.scss";`, // puede usar este scss en todos los archivos
