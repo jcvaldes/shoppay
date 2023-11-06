842ff150f145c40aa470a11e620c474d76568441
Juan Carlos Valdés <idevkingos@gmail.com>
2023-11-06 09:48:04 -0300

    nextauth

--- next.config.js
+++ next.config.js
@@ -6,21 +6,6 @@ const nextConfig = {
     removeConsole: false,
   },
   swcMinify: true,
-  async rewrites() {
-    return [
-      {
-        source: `/api/:path*`,
-        destination: `/api/:path*`,
-      },
-      {
-        source: `/:path*`,
-        destination: `${process.env.NEXTAUTH_URL}/:path*`,
-      },
-    ]
-  },
-  env: {
-    NEXTAUTH_URL: 'https://devkingos-shopay-dev.vercel.app',
-  },
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
     prependData: `@import "./base.scss";`, // puede usar este scss en todos los archivos
