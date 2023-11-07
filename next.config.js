dc07ae3f471b7df597fc2f771f572af011eab842
Juan Carlos Valdés <idevkingos@gmail.com>
2023-11-06 22:26:24 -0300

    add deploy github actions

--- next.config.js
+++ next.config.js
@@ -2,26 +2,26 @@
 const path = require('path')
 const nextConfig = {
   reactStrictMode: true,
-  compiler: {
-    removeConsole: false,
-  },
+  // compiler: {
+  //   removeConsole: false,
+  // },
   swcMinify: true,
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
     prependData: `@import "./base.scss";`, // puede usar este scss en todos los archivos
   },
-  async rewrites() {
-    return [
-      {
-        source: `/api/:path*`,
-        destination: `/api/:path*`,
-      },
-      {
-        source: `/:path*`,
-        destination: `${process.env.NEXTAUTH_URL}/:path*`,
-      },
-    ]
-  },
+  // async rewrites() {
+  //   return [
+  //     {
+  //       source: `/api/:path*`,
+  //       destination: `/api/:path*`,
+  //     },
+  //     {
+  //       source: `/:path*`,
+  //       destination: `${process.env.NEXTAUTH_URL}/:path*`,
+  //     },
+  //   ]
+  // },
   env: {
     NEXTAUTH_URL: `${process.env.NEXTAUTH_URL}`,
   },
