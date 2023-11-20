94db5eeab4d77a3131e9a32ac0ee887a1cdc1693
Juan Carlos Valdés <idevkingos@gmail.com>
2023-11-20 16:31:24 -0300

    console logs

--- utils/sendEmail.ts
+++ utils/sendEmail.ts
@@ -42,48 +42,52 @@ export const sendEmail = async (
     MAILING_SERVICE_REFRESH_TOKEN,
   })
   console.log('get access token')
-  const accessToken = await oauth2Client.getAccessToken()
-  console.log({ accessToken })
+  try {
+    const accessToken = await oauth2Client.getAccessToken()
+    console.log({ accessToken })
 
-  console.log('create transporter')
-  const transporter = nodemailer.createTransport(
-    smtpTransport({
-      service: 'gmail',
-      host: 'smtp.gmail.com',
-      port: 587,
-      secure: false,
-      auth: {
-        type: 'OAuth2',
-        user: SENDER_EMAIL_ADDRESS,
-        // si la cuenta de google esta configurada para usar oauth2
-        // para permitir aplicaciones menos seguras no esta mas disponible
-        // es necesario crear un password para aplicación.
-        pass: 'rsagnslkirqnvpwm', // rsag nslk irqn vpwm
-        clientId: MAILING_SERVICE_CLIENT_ID,
-        clientSecret: MAILING_SERVICE_CLIENT_SECRET,
-        // para cambiarlo ir a https://developers.google.com/oauthplayground y
-        // ver el classroom que esta explicado en la seccion de nextjs
-        refreshToken: MAILING_SERVICE_REFRESH_TOKEN,
-        accessToken: accessToken.token!,
-      },
-    }),
-  )
-  const mailOptions = {
-    from: SENDER_EMAIL_ADDRESS,
-    to: to,
-    subject: subject,
-    html: template(to, url),
-  }
-  console.log('sendemail')
-  console.log(mailOptions)
-  transporter.sendMail(mailOptions, (err, infos) => {
-    console.log({ err })
-    if (err) {
-      console.log(err)
-      return true
-    } else {
-      console.log(infos.response)
-      return false
+    console.log('create transporter')
+    const transporter = nodemailer.createTransport(
+      smtpTransport({
+        service: 'gmail',
+        host: 'smtp.gmail.com',
+        port: 587,
+        secure: false,
+        auth: {
+          type: 'OAuth2',
+          user: SENDER_EMAIL_ADDRESS,
+          // si la cuenta de google esta configurada para usar oauth2
+          // para permitir aplicaciones menos seguras no esta mas disponible
+          // es necesario crear un password para aplicación.
+          pass: 'rsagnslkirqnvpwm', // rsag nslk irqn vpwm
+          clientId: MAILING_SERVICE_CLIENT_ID,
+          clientSecret: MAILING_SERVICE_CLIENT_SECRET,
+          // para cambiarlo ir a https://developers.google.com/oauthplayground y
+          // ver el classroom que esta explicado en la seccion de nextjs
+          refreshToken: MAILING_SERVICE_REFRESH_TOKEN,
+          accessToken: accessToken.token!,
+        },
+      }),
+    )
+    const mailOptions = {
+      from: SENDER_EMAIL_ADDRESS,
+      to: to,
+      subject: subject,
+      html: template(to, url),
     }
-  })
+    console.log('sendemail')
+    console.log(mailOptions)
+    transporter.sendMail(mailOptions, (err, infos) => {
+      console.log({ err })
+      if (err) {
+        console.log(err)
+        return true
+      } else {
+        console.log(infos.response)
+        return false
+      }
+    })
+  } catch (err) {
+    console.log({ err })
+  }
 }
